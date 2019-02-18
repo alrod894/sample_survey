c5305f5fcfefbc3e1d0a65c1d6f61672272d935f
alrod <alberto@bitlab.mx>
2019-02-18 15:22:38 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surv::Application.config.secret_key_base = '7e7bc16e120de178e63358c00b7f7fd0bfc563eee6ef03b3ba44b488dfb0af87609ebd5d7f48b9aafa9c1dc52a74bd53c4f126ab7e0bb4b0acb760a482fa940b'
